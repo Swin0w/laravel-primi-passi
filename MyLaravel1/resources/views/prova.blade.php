a1df09fcb345e81f32b8703e64a7db7dbcfdfa2d
Simone <marzolla.istruzione@gmail.com>
2021-05-17 18:07:05 +0200

    Esercizio

---
+++ MyLaravel1/resources/views/prova.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="it-IT" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+
+  </head>
+  <body>
+    <p>aaaaa</p>
+  </body>
+</html>
